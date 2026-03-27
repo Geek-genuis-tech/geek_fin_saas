d7298810bbec838fe3015225e5ca321903183b06
Misamu12 <patrickmisamu77@gmail.com>
2026-03-27 04:16:52 +0100

    Backend : db modification

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME, 
-  port: Number(process.env.DB_PORT) || 3306,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "geekfin", // <- ta base
+  port: Number(process.env.DB_PORT) || 3001,
 });
 
 export default db;
--- backend/src/test-db.ts
+++ backend/src/test-db.ts
@@ -1,14 +1,23 @@
-// src/test-db.ts
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-import db from "./config/db";
+dotenv.config();
 
-async function testDB() {
+async function test() {
   try {
-    const [rows] = await db.query("SELECT 0");
-    console.log("Connexion MySQL réussie :", rows);
-  } catch (error) {
-    console.error("Erreur connexion DB :", error);
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT) || 3306,
+    });
+    const [rows] = await connection.query("SELECT NOW() as now");
+    console.log(rows);
+    await connection.end();
+  } catch (err) {
+    console.error("Erreur connexion DB:", err);
   }
 }
 
-testDB();
+test();
